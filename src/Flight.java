29234481b1143ac100145f4cb153022951f2722e
Eric Su <esu@usc.edu>
2018-09-12 00:03:07 -0700

    first commit

---
+++ src/Flight.java
@@ -0,0 +1,10 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Flight {
+	public static void main(String [] args) {
+	
+	}
+}
