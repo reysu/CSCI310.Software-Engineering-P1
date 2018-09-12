4b32b08e6176e736a140cd764e11e899590d8291
Eric Su <esu@usc.edu>
2018-09-12 00:32:12 -0700

    added file writing function

--- src/SearchMap.java
+++ src/SearchMap.java
@@ -1,4 +1,9 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class SearchMap {
@@ -6,18 +11,23 @@ public class SearchMap {
 	public static void main(String args[]) {
 		if(0 < args.length) {
 			String inputFileName = args[0] + ".txt";
-		//	File inputFile = new File(inputFileName);
-		//	String outputFileName = args[1];
-		//	File outputFile = new File(outputFileName);
-			
+			String outputFileName = args[1] + ".txt";
 			try {
 				Scanner inputReader = new Scanner(new FileInputStream(inputFileName));
 				while(inputReader.hasNextLine()) {
 					System.out.println(inputReader.nextLine());
 				}
 				inputReader.close();
+				try {
+					FileWriter output = new FileWriter(new File(outputFileName));
+					PrintWriter printWriter = new PrintWriter(output);
+					printWriter.write("Destination\t Flight Route from P\t Total Cost");
+					printWriter.close();
+					
+				} catch (IOException e) {
+				} 
 			} catch (FileNotFoundException e) {
-				System.out.println("no");
+			
 			}
 		}else {
 			System.out.println("Please enter a input file.");
