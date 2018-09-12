759ce1ca32ea19b80ed2ecafad55246a97f958fa
Eric Su <esu@usc.edu>
2018-09-12 00:25:44 -0700

    added file i/o function

--- src/SearchMap.java
+++ src/SearchMap.java
@@ -1,4 +1,26 @@
+import java.io.*;
+import java.util.Scanner;
 
 public class SearchMap {
-
+	
+	public static void main(String args[]) {
+		if(0 < args.length) {
+			String inputFileName = args[0] + ".txt";
+		//	File inputFile = new File(inputFileName);
+		//	String outputFileName = args[1];
+		//	File outputFile = new File(outputFileName);
+			
+			try {
+				Scanner inputReader = new Scanner(new FileInputStream(inputFileName));
+				while(inputReader.hasNextLine()) {
+					System.out.println(inputReader.nextLine());
+				}
+				inputReader.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("no");
+			}
+		}else {
+			System.out.println("Please enter a input file.");
+		}
+	}
 }
