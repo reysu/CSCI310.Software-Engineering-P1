def4094c6cafcd184d01a62f0e92460dda741da3
Eric Su <esu@usc.edu>
2018-09-13 21:11:30 -0700

    storeMap now stores adjaceny matrix of input

--- src/FlightMap.java
+++ src/FlightMap.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 
 public class FlightMap {
 	private int numLocations;
-	private boolean[][] flights; 
+	private int[][] flights; 
 	private List<Character> cities; 
 	/* Performs DFS to populate numLocations */
 	public FlightMap(String inputFile) throws IOException {
-		//numLocations = locations;
-		//setFlights(new boolean[numLocations][numLocations]);
 		cities = new ArrayList<Character>();
 		Scanner inputReader = new Scanner(inputFile);
 		String currLine;
@@ -25,18 +23,48 @@ public class FlightMap {
 					cities.add(currChar);
 				}
 			}
-			
 		}
+		flights = new int[numLocations][numLocations];
+		storeMap(inputFile);
 		inputReader.close();
+	}
+	private void storeMap(String inputFile) {
+		Scanner inputReader = new Scanner(inputFile);
+		String currLine;
+		while(inputReader.hasNextLine()){
+			currLine = inputReader.nextLine();
+			char currChar;
+			int cityFrom,cityTo;
+			cityFrom = cities.indexOf(currLine.charAt(0));
+			cityTo = cities.indexOf(currLine.charAt(2));
+			Scanner s = new Scanner(currLine);
+//			System.out.println(s.nextInt());
+			flights[cityFrom][cityTo] = Integer.parseInt(currLine.replaceAll("[\\D]", ""));
+		}
+		
+		/*
+		System.out.print("   ");
+		for(int i=0;i<numLocations;i++) {
+			System.out.print(cities.get(i) + "  ");
+		}
+		System.out.println("");
+		for(int i=0;i<numLocations;i++) {
+			System.out.print(cities.get(i) + "  ");
+			for(int j=0;j<numLocations;j++) {
+				System.out.print(flights[i][j] + "  ");
+			}
+			System.out.println("");
+		}
+		*/
 	}
 	public int getNumLocations() {
 		return numLocations; 
 	}
-	public boolean[][] getFlights() {
+	public int[][] getFlights() {
 		return flights;
 	}
 
-	public void setFlights(boolean[][] flights) {
+	public void setFlights(int[][] flights) {
 		this.flights = flights;
 	}
 	
